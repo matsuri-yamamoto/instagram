0895875c043e5f84f1b53891ddff10a0e21db5ef
rino.yamamoto <rino.matsushima@gmail.com>
2022-02-17 16:27:26 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 山本梨野 on 2022/02/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
